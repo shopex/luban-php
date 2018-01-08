f24c7532263b61eb17e952bbf710d958bb4bc9bb
张学会 <zhangxuehui@shopex.cn>
2018-01-08 10:58:57 +0800

    fix bug

--- src/Client.php
+++ src/Client.php
@@ -6,18 +6,18 @@ class Client{
 	static private $cache = [];
 
     public static function __callStatic($name, $arguments){    	
-    	if(self::$cache[$name]){
-	    	$opts = [];
+    	if(!isset(self::$cache[$name]) ){
+	    	$opts = $arguments[1];
 	    	$channel = null;
 	    	$host = $arguments[0];
 
-	    	$class_name = 'Shopex\\Luban\\'.$name.'Client';
+	    	$class_name = 'Shopex\\Luban\\Sdf\\'.$name.'Client';
 	    	$obj = new $class_name($host, $opts, $channel);
 
-	    	self::$cache[$obj] = $obj;
+	    	self::$cache[$name] = $obj;
     	}
 
-    	return self::$cache[$obj];
+    	return self::$cache[$name];
     }
 
 }
