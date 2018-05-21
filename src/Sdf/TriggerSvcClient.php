93d2bd91839f103fac184e50d12d7f25786f5399
jenkins <ci@shopex.cn>
2018-05-21 14:54:31 +0800

    protos update

---
+++ src/Sdf/TriggerSvcClient.php
@@ -0,0 +1,71 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Shopex\Luban\Sdf;
+
+/**
+ */
+class TriggerSvcClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Shopex\Luban\Sdf\Trigger $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Create(\Shopex\Luban\Sdf\Trigger $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/luban.TriggerSvc/Create',
+        $argument,
+        ['\Shopex\Luban\Sdf\TriggerCreateResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Shopex\Luban\Sdf\TriggerUpdateRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Update(\Shopex\Luban\Sdf\TriggerUpdateRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/luban.TriggerSvc/Update',
+        $argument,
+        ['\Shopex\Luban\Sdf\TriggerUpdateResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Shopex\Luban\Sdf\TriggerInfoRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function TriggerInfo(\Shopex\Luban\Sdf\TriggerInfoRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/luban.TriggerSvc/TriggerInfo',
+        $argument,
+        ['\Shopex\Luban\Sdf\Trigger', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Shopex\Luban\Sdf\SearchRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function TriggerList(\Shopex\Luban\Sdf\SearchRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/luban.TriggerSvc/TriggerList',
+        $argument,
+        ['\Shopex\Luban\Sdf\TriggerListResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
